29f76b59c06333f9a57dba72955b86affdb42b88
rahil <rahil@examle.com>
2018-10-24 09:36:32 +0530

    New Java Code

--- sample.java
+++ sample.java
@@ -1,6 +1,7 @@
 public class sample {
 	public static void main (String[] args) {
 		System.out.println("Welcome to Java Programming");
+		System.out.println("Code Developed for integrating GitHub");
 	}
 }
 
