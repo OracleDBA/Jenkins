316a5b6ad51a938e156ca2bb2c9165d22216d37f
rahil <rahil@examle.com>
2018-10-24 09:41:14 +0530

    Modified Java Code

--- sample.java
+++ sample.java
@@ -1,7 +1,7 @@
 public class sample {
 	public static void main (String[] args) {
 		System.out.println("Welcome to Java Programming");
-		System.out.println("Code Developed for integrating GitHub");
+		System.out.println("Integrating GitHub to access Java Code");
 	}
 }
 
